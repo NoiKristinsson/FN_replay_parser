0299402d8679d1884dd89f74a53b4c8ced97f46b
NoiKristinsson <NoiKristinsson@users.noreply.github.com>
2025-03-10 01:18:03 +0000

    uppfæra program.cs í API til að implementa port 5056 betur

--- FN_vefskorari_API/Program.cs
+++ FN_vefskorari_API/Program.cs
@@ -1,7 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+// Force the API to use port 5056
+builder.WebHost.UseUrls("http://0.0.0.0:5056");
 
+builder.Services.AddControllers();
 // Enable CORS to allow requests from Blazor WebAssembly
 builder.Services.AddCors(options =>
 {
